2fa0e469275f1dcf7e31a218a880f23575f6c658
Emir Beşir <emirbesirx@gmail.com>
2026-03-02 01:51:45 +0300

    create additional resources and adjust materials

--- goblin-gold-mine/Assets/_Project/GoblinMine/Game/Resource/Configuration/ResourceConfiguration.cs
+++ goblin-gold-mine/Assets/_Project/GoblinMine/Game/Resource/Configuration/ResourceConfiguration.cs
@@ -8,16 +8,22 @@ namespace _Project.GoblinMine.Game.Resource.Configuration
         fileName = "ResourceConfiguration")]
     public class ResourceConfiguration : ScriptableObject
     {
+        [Header("General")]
         [SerializeField] private ResourceType resourceType;
+        [Header("Visual")]
         [SerializeField] private string displayName;
         [SerializeField] private Material material;
-        [SerializeField] private int value;
+        [Header("Economy")]
+        [SerializeField] private int goldValue;
+        [Header("Collection")]
+        [SerializeField] private int collectionAmount;
         [SerializeField] private float collectionIntervalSeconds;
 
         public ResourceType ResourceType => resourceType;
         public string DisplayName => displayName;
         public Material Material => material;
-        public int Value => value;
+        public int GoldValue => goldValue;
+        public int CollectionAmount => collectionAmount;
         public float CollectionIntervalSeconds => collectionIntervalSeconds;
     }
 }
